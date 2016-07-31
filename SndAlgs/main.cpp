97f4c8c259d9405b5f0d256ef48bc82de9460415
lykanthrop <lykanthropen@gmail.com>
2016-07-31 12:28:55 +0200

    Initial Commit

---
+++ SndAlgs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SndAlgs
+//
+//  Created by lykanthrop on 2016.07.31.
+//  Copyright © 2016 varo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
